7ef8779cbd95b696a7f667dbc4066683a15f4752
Svetlana-SO-ops <svetusiksss1995@gmail.com>
2024-07-31 16:02:04 +0300

    Добавила файлы

---
+++ configuration.py
@@ -0,0 +1,5 @@
+# URL_SERVICE хранит базовый URL веб-сервиса, который используется для доступа к API или другим ресурсам
+URL_SERVICE = "https://070dd66d-8835-4d87-879f-554918b0d694.serverhub.praktikum-services.ru"
+
+# CREATE_ORDERS хранит путь к API-методу для создания нового заказа.
+CREATE_ORDERS = "/api/v1/orders"
